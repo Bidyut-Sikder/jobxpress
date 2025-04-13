a48cc7ec5a111d3d059e1579649c5afe42573baa
bidyut-sikder <bidyutsikder2001@gmail.com>
2025-04-13 22:43:27 +0600

    feat: adding dark mode theme

--- components/general/Navbar.tsx
+++ components/general/Navbar.tsx
@@ -1,13 +1,23 @@
 import Link from "next/link";
 import React from "react";
+import logo from "@/public/logo.png";
+import Image from "next/image";
+import { Button } from "../ui/button";
+import { ThemeToggle } from "./ThemeToggle";
 
 function Navbar() {
   return (
     <nav className="flex justify-between items-center py-5">
-      <Link href="/" className="text-2xl font-bold text-gray-800">
-       <h1 className="text-2xl font-bold">Job<span className="text-primary ">Xpress</span></h1>
+      <Link href="/" className=" flex items-center gap-2">
+        <Image src={logo} alt="Logo" width={40} height={40} />
+        <h1 className="text-2xl font-bold">
+          Job<span className="text-primary ">Xpress</span>
+        </h1>
       </Link>
 
+      <Button>bidyut</Button>
+
+      <ThemeToggle />
     </nav>
   );
 }
