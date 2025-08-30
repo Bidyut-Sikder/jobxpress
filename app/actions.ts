7f645b9b8e0347383b196d13fc929815966bfc3e
bidyut-sikder <bidyutsikder2001@gmail.com>
2025-08-30 13:37:02 +0600

    `feat:  arcjet  setup for protection from different attacks `

--- app/actions.ts
+++ app/actions.ts
@@ -1,15 +1,38 @@
 "use server";
 
+import arcjet, { detectBot, shield } from "@/lib/arcjet";
 import { prisma } from "@/lib/db";
 import { requireUser } from "@/lib/requireUser";
 import { companySchema, jobSeekerSchema } from "@/lib/zodSchemas";
-import { redirect } from "next/navigation";
+import { request } from "@arcjet/next";
 
+import { redirect } from "next/navigation";
 import { z } from "zod";
+
+const aj = arcjet
+  .withRule(
+    shield({
+      mode: "LIVE",
+    })
+  )
+  .withRule(
+    detectBot({
+      mode: "LIVE",
+      allow: [
+        // "CATEGORY:SEARCH_ENGINE"
+      ],
+    })
+  );
 
 export const createCompany = async (data: z.infer<typeof companySchema>) => {
   const session = await requireUser();
 
+  const req = await request();
+
+  const decision = await aj.protect(req);
+  if (decision.isDenied()) {
+    throw new Error("Forbidden");
+  }
   const validateData = companySchema.parse(data);
 
   await prisma.user.update({
@@ -29,9 +52,15 @@ export const createCompany = async (data: z.infer<typeof companySchema>) => {
   return redirect("/");
 };
 
-export const createJobSeeker = async (data: z.infer<typeof jobSeekerSchema>) => {
+export const createJobSeeker = async (
+  data: z.infer<typeof jobSeekerSchema>
+) => {
   const session = await requireUser();
-
+  const req = await request();
+  const decision = await aj.protect(req);
+  if (decision.isDenied()) {
+    throw new Error("Forbidden");
+  }
   const validateData = jobSeekerSchema.parse(data);
 
   await prisma.user.update({
