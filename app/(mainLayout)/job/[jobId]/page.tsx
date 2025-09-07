efaac6fb849fb1c9ae17d4ea200c68e1f74b764d
bidyut-sikder <bidyutsikder2001@gmail.com>
2025-09-07 10:37:52 +0600

    feat: single job page is done

--- app/(mainLayout)/job/[jobId]/page.tsx
+++ app/(mainLayout)/job/[jobId]/page.tsx
@@ -2,10 +2,12 @@ import { benefits } from "@/components/general/BenefitsSelector";
 import JsonToHtml from "@/components/general/JsonToHtml";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
 import { prisma } from "@/lib/db";
 import { getFlagEmoji } from "@/lib/utils";
 
 import { Heart } from "lucide-react";
+import Image from "next/image";
 import { notFound } from "next/navigation";
 import React from "react";
 
@@ -24,6 +26,7 @@ const getJob = async (jobId: string) => {
       location: true,
       benefits: true,
       createdAt: true,
+      listingDuration: true,
       company: {
         select: {
           name: true,
@@ -45,8 +48,8 @@ const JobIdPage = async ({ params }: { params: Params }) => {
   const job = await getJob(jobId);
 
   return (
-    <div className="grid grid-cols-[1fr,400px] gap-4 bg-amberg-300">
-      <div className="space-y-8">
+    <div className="grid lg:grid-cols-3 gap-8 bg-amberr-300">
+      <div className="space-y-8 col-span-2">
         <div className="flex items-center justify-between">
           <div className="">
             <h1 className="text-3xl font-bold "> {job.jobTitle}</h1>
@@ -77,16 +80,12 @@ const JobIdPage = async ({ params }: { params: Params }) => {
           <div className="flex flex-wrap gap-3">
             {benefits.map((benefit) => {
               const isOffered = job.benefits.includes(benefit.id);
-     
 
               return (
                 <Badge
-                  //   onClick={() => toggleBenefit(benefit.id)}
-                  className="cursor-pointer transition-all hover:scale-105
-                         active:scale-95 text-sm px-4 py-1.5 rounded-full"
+                  className="cursor-not-allowed text-sm px-4 py-1.5 rounded-full"
                   key={benefit.id}
                   variant={isOffered ? "default" : "outline"}
-                  // variant={"outline"}
                 >
                   <span className="flex items-center gap-2">
                     {benefit.icon}
@@ -97,6 +96,91 @@ const JobIdPage = async ({ params }: { params: Params }) => {
             })}
           </div>
         </section>
+      </div>
+
+      <div className="space-y-6 col-span-1">
+        <Card className="p-6">
+          <div className="space-y-4">
+            <div>
+              <h3 className=" font-semibold">Apply Now</h3>
+              <p className="text-sm text-muted-foreground mt-1">
+                Submit your application today and take the next step in your
+                career. Our team will review your profile and get back to you
+                shortly.
+              </p>
+            </div>
+            <Button className="w-full">Apply Now</Button>
+          </div>
+        </Card>
+        <Card className="p-6">
+          <h3 className="font-semibold ">About The Job</h3>
+          <div className="space-y-2">
+            <div className="flex justify-between">
+              <span className="text-sm text-muted-foreground">
+                Apply Before
+              </span>
+              <span className="text-sm ">
+                {new Date(
+                  job.createdAt.getTime() +
+                    job.listingDuration * 24 * 60 * 60 * 1000
+                ).toLocaleDateString("en-US", {
+                  month: "long",
+                  day: "numeric",
+                  year: "numeric",
+                })}
+              </span>
+            </div>
+
+            <div className="flex justify-between">
+              <span className="text-sm text-muted-foreground ">Posted on</span>
+              <span className="text-sm">
+                {job.createdAt.toLocaleDateString("en-US", {
+                  month: "long",
+                  day: "numeric",
+                  year: "numeric",
+                })}
+              </span>
+            </div>
+            <div className="flex justify-between">
+              <span className="text-sm text-muted-foreground ">
+                Employment Type
+              </span>
+              <span className="text-sm">
+                {job.employmentType.toUpperCase()}
+              </span>
+            </div>
+            <div className="flex justify-between">
+              <span className="text-sm text-muted-foreground ">Location</span>
+              <span className="text-sm">
+                {getFlagEmoji(job.location) && (
+                  <span className="mr-1">{getFlagEmoji(job.location)}</span>
+                )}{" "}
+                {job.location}
+              </span>
+            </div>
+          </div>
+        </Card>
+
+        {/* company details */}
+        <Card className="p-6">
+          <div className="space-y-4">
+            <div className="flex items-center gap-3">
+              <Image
+                src={job.company.logo}
+                className="rounded-full size-12 "
+                width={48}
+                height={48}
+                alt="company logo"
+              />
+              <div className="flex flex-col ">
+                <h3 className="font-semibold"> {job.company.name}</h3>
+                <p className="text-sm text-muted-foreground line-clamp-2">
+                  {job.company.about}
+                </p>
+              </div>
+            </div>
+          </div>
+        </Card>
       </div>
     </div>
   );
